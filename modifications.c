d1184aeaf48262efe63a384b0fb046f1132d7921
mayhm28 <may.hamrouni@ensi-uma.tn>
2025-11-25 21:48:21 +0100

    Update modifications.c

--- modifications.c
+++ modifications.c
@@ -22,14 +22,7 @@ main(){
 			compteur=0;
 		}	
 		//affichage//
-		if (compteur>=consec && m.niv_alerte !=0){
-			printf(" [%s] Temperature: %.2f°C ---- Alerte niveau : %d --- DECLENCHEMENT DE L'ALARME \n", m.date,m.temp,m.niv_alerte);
-			
-		}else if(m.niv_alerte !=0) {
-			printf(" [%s] Temperature: %.2f°C ---- Alerte niveau : %d (consécutives=%d) --- alarme en attente \n", m.date,m.temp,m.niv_alerte,compteur);			
-		}else{
-			printf("[%s] Temperature: %.2f°C --- Normal \n", m.date, m.temp);
-		}
+		afficher_mesure(m, compteur, consec);
 		journal[i]=m;
 		ecrire_journal(m);//fonction qui ecrit un enregistrement dans le fichier journaling//
 		i++;
@@ -50,4 +43,5 @@ main(){
 	printf(" \n Programme terminé. Rapport generé(%d mesures) .\n" ,i);
 	return 0;
 	
+
 
