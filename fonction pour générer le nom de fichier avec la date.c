0a8c9954c5df94b40dddce6092e01dbdfc898517
mayhm28 <may.hamrouni@ensi-uma.tn>
2025-11-25 21:13:49 +0100

    Update fonction pour générer le nom de fichier avec la date.c

--- fonction pour générer le nom de fichier avec la date.c
+++ fonction pour générer le nom de fichier avec la date.c
@@ -1,7 +1,8 @@
 void nom_fichier_rapport(char *nom_fichier) {
     char d[100];
     date(d);
-    sprintf(nom_fichier, "rapport_%.10s.txt", d)
+    sprintf(nom_fichier, "rapport_%.10s.txt", d);
 }
+
 
 
