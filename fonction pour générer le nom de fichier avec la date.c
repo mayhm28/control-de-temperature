403af4dde433c473744fce2e45b18d3249510ab6
imenebenchikha <imen.ben.chikhann@gmail.com>
2025-11-25 21:06:54 +0100

    Update fonction pour générer le nom de fichier avec la date.c

--- fonction pour générer le nom de fichier avec la date.c
+++ fonction pour générer le nom de fichier avec la date.c
@@ -1,5 +1,7 @@
 void nom_fichier_rapport(char *nom_fichier) {
-    time_t now = time(NULL);             // récupère le temps actuel
-    struct tm *t = localtime(&now);      // convertit en date/heure locale
-    sprintf(nom_fichier, "rapport_%04d-%02d-%02d.txt", t->tm_year + 1900, t->tm_mon + 1, t->tm_mday);
+    char d[100];
+    date(d);
+    d[10]='\0'
+    sprintf(nom_fichier, "rapport_%s.txt", d);
 }
+
