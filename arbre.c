e3fafc9a149542faa0efb2be2de9722db6c38df8
imenebenchikha <imen.ben.chikhann@gmail.com>
2025-12-07 16:28:28 +0100

    Update arbre.c

--- arbre.c
+++ arbre.c
@@ -32,3 +32,11 @@ void inorder(arbre* racine){
 	printf("[%s] Temperature: %.2f°C ---- Alerte niveau : %d ---\n",racine->val.date,racine->val.temp,racine->val.niv_alerte);
 	inorder(racine->droite);
 }
+void liberer(arbre* racine) {
+    if (racine == NULL){
+		return;
+	}
+    liberer(racine->gauche);
+    liberer(racine->droite);
+    free(racine);
+}
