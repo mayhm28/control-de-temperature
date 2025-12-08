77bf85e7117edead25ce97656e3e6146692d844d
imenebenchikha <imen.ben.chikhann@gmail.com>
2025-12-08 23:02:08 +0100

    Update modification rapport.c

--- modification rapport.c
+++ modification rapport.c
@@ -44,9 +44,9 @@ void compter_alertes(arbre* A, int* a1, int* a2, int* a3) {
 }
 
 // Générer le rapport à partir de l'arbre
-void generer_rapport_arbre(arbre* A, int n, char *nom_fichier) {
+void generer_rapport(mesure journal[], arbre* A, int n, char *nom_fichier) {
     if (A == NULL || n == 0) {
-        printf("Aucune mesure dans l'arbre. Rapport non généré.\n");
+        printf("Aucune mesure. Rapport non généré.\n");
         return;
     }
 
@@ -55,19 +55,25 @@ void generer_rapport_arbre(arbre* A, int n, char *nom_fichier) {
         printf("Impossible de créer %s\n", nom_fichier);
         return;
     }
+    for (int i = 0; i < n; i++){
+        fprintf(f,"%s | %.2f | %d\n",journal[i].date,journal[i].temp,journal[i].niv_alerte);
+    }
+
+    fprintf(f, "\n");
 
     float min_val = min(A)->val.temp;
     float max_val = max(A)->val.temp;
     float somme_val = somme(A);
     float moyenne = somme_val / n;
 
-    int a1=0, a2=0, a3=0;
+    int a1 = 0, a2 = 0, a3 = 0;
     compter_alertes(A, &a1, &a2, &a3);
 
+    
     fprintf(f, "Température minimale : %.2f°C\n", min_val);
     fprintf(f, "Température maximale : %.2f°C\n", max_val);
     fprintf(f, "Température moyenne : %.2f°C\n", moyenne);
-    fprintf(f, "Alertes : niv1=%d niv2=%d niv3=%d\n", a1, a2, a3);
+    fprintf(f, "Nombre d'alertes : niv1=%d niv2=%d niv3=%d\n", a1, a2, a3);
 
     fclose(f);
 }
